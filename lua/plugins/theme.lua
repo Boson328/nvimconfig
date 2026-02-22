da28eeae9c03d431848842d31181fc0ed99be6eb
Boson <121548464+Boson328@users.noreply.github.com>
2026-02-22 14:11:05 +0900

    テーマの若干修正

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -10,12 +10,18 @@ return {
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
 			integrations = {
 				snacks = true, -- Snacks.nvim のインテグレーション
-				telescope = {
-					enabled = true,
-				},
 				native_lsp = {
 					enabled = true,
 				},
+			},
+			highlight_overrides = {
+				all = function(colors)
+					return {
+						FloatBorder = { bg = colors.none },
+						NormalFloat = { bg = colors.none },
+						FloatTitle = { bg = colors.none },
+					}
+				end,
 			},
 		},
 	},
