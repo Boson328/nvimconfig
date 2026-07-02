9b84452a3fa4d83c4c541f05983ccd2e273cb2a6
boson328 <boson_typing@outlook.jp>
2026-07-03 08:50:55 +0900

    commit

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
 -- lazy.nvim のブートストラップ
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
 vim.opt.rtp:prepend(lazypath)
 
 -- 基本オプションの設定
