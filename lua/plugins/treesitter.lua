2e422440defca965d1107f6bfb1197d62caf717c
boson328 <boson_typing@outlook.jp>
2026-07-02 19:20:50 +0900

    commit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 		branch = "master",
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
+		commit = "v0.9.3",
 		config = function()
 			local install_dir = vim.fn.stdpath("data") .. "/site"
 			vim.opt.runtimepath:prepend(install_dir) -- ← 追加
