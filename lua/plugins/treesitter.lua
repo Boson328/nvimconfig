942e9ff3bf709679f3ef7f4970697951caaa7342
boson328 <boson_typing@outlook.jp>
2026-07-03 20:38:13 +0900

    commit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,27 +3,21 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		branch = "main",
 		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
+		lazy = false,
 		config = function()
-			local install_dir = vim.fn.stdpath("data") .. "/site"
-			vim.opt.runtimepath:prepend(install_dir) -- ← 追加
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
 
-			local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-			if not status_ok then
-				return
-			end
+			-- パーサーのインストール
+			require("nvim-treesitter").install({ "c", "wgsl", "rust" })
 
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-			configs.setup({
-				parser_install_dir = install_dir, -- ← 追加
-				ensure_installed = { "c", "wgsl", "rust" },
-				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
+			-- ハイライトの有効化(新方式ではFileTypeで手動起動)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "c", "wgsl", "rust" },
+				callback = function()
+					vim.treesitter.start()
+				end,
 			})
 		end,
 	},
